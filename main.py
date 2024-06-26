5b041fa535384b5f5b43a3018b03c84cc5474e27
TavoRivera <octaviostyles02@gmail.com>
2024-06-25 23:03:22 -0600

    eliminamos los filtros y solo dejamos a escala de grises. Los resultados de deteccion mejoraron. la funcion main la hemos dejada comentada para verificar que es posible ejecutar el algoritmo ya entrenado

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import cv2
-from PIL import Image, ImageEnhance, ImageFilter
+from PIL import Image
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -17,11 +17,8 @@ def load_images_from_folder(folder_path):
             for filename in os.listdir(label_path):
                 img_path = os.path.join(label_path, filename)
                 try:
-
                     img = preprocess_image(img_path)
-
-
-                   # img = Image.open(img_path).convert('L')  # Convertimos a escala de grises
+                    #img = Image.open(img_path).convert('L')  # Convertimos a escala de grises
                     img = img.resize((64, 64))  # Redimensionamos las imágenes para que tengan el mismo tamaño
                     img_array = np.array(img).flatten()  # Aplanamos la imagen
                     images.append(img_array)
@@ -41,32 +38,14 @@ def preprocess_image(image_path):
     # Abre la imagen usando Pillow y conviértela a un formato que OpenCV puede usar
     original_image = Image.open(image_path)
     image_cv = cv2.cvtColor(np.array(original_image), cv2.COLOR_RGB2BGR)
-
     # Convertir a escala de grises
     gray_image = cv2.cvtColor(image_cv, cv2.COLOR_BGR2GRAY)
-
-    # Aplicar filtro de mediana para reducir el ruido
-    median_filtered_image = cv2.medianBlur(gray_image, 1)
-
-    # Aplicar filtro Gaussian blur para suavizar la imagen
-    gaussian_blur_image = cv2.GaussianBlur(median_filtered_image, (1, 1), 1)
-
-    # Aplicar filtro de Sobel para detectar bordes
-    sobelx = cv2.Sobel(gaussian_blur_image, cv2.CV_64F, 1, 0, ksize=1)
-    sobely = cv2.Sobel(gaussian_blur_image, cv2.CV_64F, 0, 1, ksize=1)
-    sobel_combined = cv2.addWeighted(cv2.convertScaleAbs(sobelx), 2, cv2.convertScaleAbs(sobely), 2, 0)
-
-    # Aplicar umbralización para segmentación de objetos
-    ret, threshold_image = cv2.threshold(sobel_combined, 127, 255, cv2.THRESH_BINARY)
-
     # Convierte la imagen procesada de OpenCV a Pillow y guárdala si es necesario
-    final_image = Image.fromarray(threshold_image)
+    final_image = Image.fromarray(gray_image)
     # if save_path:
     #     final_image.save(save_path)
-    #
     # Muestra la imagen procesada
-    # final_image.show(title="Processed Image")
-
+    #final_image.show(title="Processed Image")
     return final_image
 
 def predict_new_image(image_path):
@@ -78,33 +57,33 @@ def predict_new_image(image_path):
     return prediction[0]
 
 def main():
-
-    folder_path = 'images'
-    images, labels = load_images_from_folder(folder_path)
-
-    output_csv = 'imagenes.csv'
-    images_to_csv(images, labels, output_csv)
-
-    # Cargar el CSV
-    data = pd.read_csv(output_csv)
-    X = data.drop('label', axis=1)
-    y = data['label']
-
-    # Dividir los datos en entrenamiento y prueba
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    # Entrenar un clasificador
-    clf = RandomForestClassifier(n_estimators=100, random_state=42)
-    clf.fit(X_train, y_train)
-
-    joblib.dump(clf, 'modelo_entrenado.pkl')
+    pass
+    # folder_path = 'images'
+    # images, labels = load_images_from_folder(folder_path)
+    #
+    # output_csv = 'imagenes.csv'
+    # images_to_csv(images, labels, output_csv)
+    #
+    # # Cargar el CSV
+    # data = pd.read_csv(output_csv)
+    # X = data.drop('label', axis=1)
+    # y = data['label']
+    #
+    # # Dividir los datos en entrenamiento y prueba
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    #
+    # # Entrenar un clasificador
+    # clf = RandomForestClassifier(n_estimators=100, random_state=42)
+    # clf.fit(X_train, y_train)
+    #
+    # joblib.dump(clf, 'modelo_entrenado.pkl')
     # # Hacer predicciones
     # y_pred = clf.predict(X_test)
     #
     # # Evaluar el modelo
     # accuracy = accuracy_score(y_test, y_pred)
     # print(f'Precisión del modelo: {accuracy}')
-    #
+    # #
     # # Generar el reporte de clasificación
     # report = classification_report(y_test, y_pred)
     # print('Reporte de Clasificación:')
@@ -117,6 +96,6 @@ def main():
 
 if __name__ == '__main__':
     main()
-    nueva_imagen_path = '3.jpg'
+    nueva_imagen_path = '9.png'
     prediccion = predict_new_image(nueva_imagen_path)
     print(f'La predicción para la nueva imagen es: {prediccion}')
